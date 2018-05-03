9de1b8c5a060d46b79add4b395ab1c1b210e52b7
Zongheng Wang <wangzonghen9@gmail.com>
2018-05-03 16:28:50 -0400

    add start scene

---
+++ Prototype_2_group/Assets/Scripts/changeTransparency.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeTransparency : MonoBehaviour {
+	private SpriteRenderer sr;
+	private float time; //timer variable for tweening colors
+	public float startTime;
+	public Gradient colorGradient;
+	private bool rise;
+	public float changeTime = 100f;
+	// Use this for initialization
+	void Start () {
+		sr = GetComponent<SpriteRenderer>();
+		time = startTime;
+		rise = false;
+		sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, (float)0.5);
+	}
+
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		sr.color = new Color(255, 255, 2555, (float)(time / changeTime));
+		//sr.color = colorGradient.Evaluate (time / changeTime); //as time increases, we move along the gradient
+		if (rise) {
+			time += Time.deltaTime;
+			if (time >= changeTime) {
+				rise = false;
+			}
+		} else {
+			time -= Time.deltaTime;
+			if (time <= 0) {
+				rise = true;
+			}
+		}
+		//Debug.Log(Time.deltaTime);
+	}
+}
---
+++ Prototype_2_group/Assets/Scripts/startSceneControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startSceneControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void gameStart(){
+		SceneManager.LoadScene ("Main 1");
+	}
+
+	public void exitGame(){
+		Application.Quit();
+	}
+}
